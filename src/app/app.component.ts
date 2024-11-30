6660b8d2494c35ff3c1819122a0bfabe0f851005
damdaph <davejustinarcilla@gmail.com>
2024-12-01 01:48:20 +0800

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+removeTask(_t11: any) {
+throw new Error('Method not implemented.');
+}
+addTask() {
+throw new Error('Method not implemented.');
+}
   title = 'Sidebar Navigation';
 }
